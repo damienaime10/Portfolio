beb9c94e392eeff475e4faf30129a4d439c805ff
damienaime10 <damienaime32@gmail.com>
2025-09-09 23:27:22 +0200

    Mofifier la couleur du cadre

--- src/components/projet.js
+++ src/components/projet.js
@@ -26,32 +26,32 @@ export default function Projects() {
 
   return (
     <section id="projects" className="py-16 bg-gray-600">
-      <div className="max-w-7xl mx-auto px-6">
-        <h2 className="text-4xl font-bold text-center mb-6 text-white">Mes Projets</h2>
+      <div className="px-6 mx-auto max-w-7xl">
+        <h2 className="mb-6 text-4xl font-bold text-center text-white">Mes Projets</h2>
 
-        <div className="grid md:grid-cols-3 gap-8">
+        <div className="grid gap-8 md:grid-cols-3">
           {projects.map((project, index) => (
             <Card 
               key={index}
               
-              className="hover:scale-105 transition-transform duration-300"
+              className="duration-300 bg-gray-700 text-whitetransition-transform hover:scale-105"
               renderImage={() => (
                 <img 
                   src={project.image} 
                   alt={project.title} 
-                  className="w-full h-56 object-cover"
+                  className="object-cover w-full h-56"
                 />
               )}
             >
               <h5 className="text-4xl font-bold tracking-tight text-white">
                 {project.title}
               </h5>
-              <p className="font-normal  text-white mb-4">
+              <p className="mb-4 font-normal text-white">
                 {project.description}
               </p>
               <a 
                 href={project.link} 
-                className="inline-block bg-blue-600 text-white px-2 py-2 rounded-lg hover:bg-blue-700 transition"
+                className="inline-block px-2 py-2 text-white transition bg-blue-600 rounded-lg hover:bg-blue-700"
               >
                 Voir le projet
               </a>
